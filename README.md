c00f18e6bcd9e5b189b5468a80376e157fcea393
Vo64 <1850041@isep.ipp.pt>
2020-11-21 22:26:21 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-fww539
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-fww539)
---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
+
+
+
+   /* Registering a new router
+    
+    Generate a new component for product details. Give the component the name product-details.
+    Reminder: In the file list, right-click the app folder, choose Angular Generator and Component.
+    In app.module.ts, add a route for product details, with a path of products/:productId and ProductDetailsComponent for the component.
+     
+    */
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+
+      { path: "", component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ])
+    
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent
+  ],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
